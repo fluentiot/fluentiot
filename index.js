598cfff9e3e0b53dff580469b2387a4cfefec67c
Darren Moore <darren.m@hyper-trade.com>
2023-11-24 17:20:54 +0700

    git ignore and renamed main fluent-iot.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Fluent IoT - The Programmers IoT Framework
+ */
+const Fluent = require('./src/fluent');
+const logger = require('./src/utils/logger');
+const components = Fluent.component().all();
+
+// Combine components and Scenario in the export
+module.exports = {
+    Fluent,
+    logger,
+    scenario: (description) => {
+        return Fluent.scenario().create(description)
+    },
+    ...components
+};
