7f002646501e14679e8400c4707dfeadab882c43
Darren Moore <darren.m@firecreekweb.com>
2023-11-27 22:52:30 +0700

    Datetime testing

--- src/components/datetime/datetime_component.js
+++ src/components/datetime/datetime_component.js
@@ -3,8 +3,6 @@ const moment = require('moment');
 const logger = require('./../../utils/logger');
 
 const Component = require('./../component');
-const DatetimeTriggers = require('./datetime_triggers');
-const DatetimeConstraints = require('./datetime_constraints');
 
 /**
  * Date time component
@@ -42,17 +40,23 @@ class DatetimeComponent extends Component {
 
     triggers(Scenario) {
         return {
-            time: () => {
-                return {
-                    is: (targetTime) => { 
-                        new DatetimeTriggers(Scenario, this.Event).is(targetTime);
-                        return Scenario.triggers;
-                    },
-                    every: (target) => {
-                        new DatetimeTriggers(Scenario, this.Event).every(target);
-                        return Scenario.triggers;
+            time: {
+                is: (targetTime) => { 
+                    this.event().on('datetime.time', (time) => {
+                        if (time === targetTime) { Scenario.assert(); }
+                    });
+                    return Scenario.triggers;
+                },
+                every: (target) => {
+                    const parsedSchedule = this._parseCronExpression(target);
+                    if (parsedSchedule === null) {
+                        throw new Error(`Failed to parse schedule for '${target}'.`);
                     }
-                };
+                    schedule.scheduleJob(parsedSchedule, () => {
+                        Scenario.assert();
+                    });
+                    return Scenario.triggers;
+                }
             },
         }
     }
@@ -68,52 +72,58 @@ class DatetimeComponent extends Component {
             },
             time: {
                 between: (targetStart, targetEnd) => {
-                    constraints.push(() => { return new DatetimeConstraints(this).between(targetStart, targetEnd); });
-                    return Scenario.constraint(constraints)
+                    if (!moment(targetStart, 'HH:mm', true).isValid()) {
+                        logger.error(`Start date "${targetStart}" is not in the correct format of HH:mm`,'datetime');
+                        return false;
+                    }
+                    if (!moment(targetEnd, 'HH:mm', true).isValid()) {
+                        logger.error(`Start date "${targetStart}" is not in the correct format of HH:mm`,'datetime');
+                        return false;
+                    }
+                    const currentTime = moment().format('HH:mm');
+                    return currentTime >= targetStart && currentTime <= targetEnd;
                 }
             }
         }
     }
 
     _parseDay(input) {
-        const weekdayMap = {
-            monday: 'Monday',
-            tuesday: 'Tuesday',
-            wednesday: 'Wednesday',
-            thursday: 'Thursday',
-            friday: 'Friday',
-        };
-    
-        const weekendMap = {
-            saturday: 'Saturday',
-            sunday: 'Sunday',
-        };
-    
-        const result = [];
+        const results = [];
     
         const parseSingleDay = (day) => {
-            let parsedDay = null;
-        
-            // Check weekdayMap
-            if (weekdayMap.hasOwnProperty(day.toLowerCase())) {
-                parsedDay = weekdayMap[day.toLowerCase()];
+            let parsedDay = [];
+            day = day.toLowerCase();
+
+            // Include weekends
+            if(day === 'weekend') {
+                parsedDay.push('Saturday');
+                parsedDay.push('Sunday');
             }
-        
-            // Check weekendMap
-            if (!parsedDay && weekendMap.hasOwnProperty(day.toLowerCase())) {
-                parsedDay = weekendMap[day.toLowerCase()];
+
+            // Include weekdays
+            if(parsedDay.length === 0 && day === 'weekday') {
+                parsedDay.push('Monday');
+                parsedDay.push('Tuesday');
+                parsedDay.push('Wednesday');
+                parsedDay.push('Thursday');
+                parsedDay.push('Friday');
             }
         
             // Check if the day is already a valid day name
-            if (!parsedDay && moment(day, 'dddd', true).isValid()) {
-                parsedDay = moment(day, 'dddd').format('dddd');
+            if (parsedDay.length === 0 && moment(day, 'ddd', true).isValid()) {
+                parsedDay.push(moment(day, 'ddd').format('dddd'));
+            }
+        
+            // Check if the day is already a valid day name
+            if (parsedDay.length === 0 && moment(day, 'dddd', true).isValid()) {
+                parsedDay.push(moment(day, 'dddd').format('dddd'));
             }
         
             // Log an error if the day couldn't be parsed
-            if (!parsedDay) {
+            if (parsedDay.length === 0) {
                 logger.error(`Could not parse day: ${day}`);
             } else {
-                result.push(parsedDay);
+                results.push(...parsedDay);
             }
         };
     
@@ -122,10 +132,38 @@ class DatetimeComponent extends Component {
         } else if (typeof input === 'string') {
             parseSingleDay(input);
         } else {
-            logger.error('Invalid input. Expected a string or an array of strings.');
+            logger.error('Invalid input. Expected a string or an array of strings.', 'datetime');
         }
     
-        return result;
+        return results;
+    }
+
+    _parseCronExpression(target) {
+        const match = target.match(/^(\d+)?\s*(?:(second|minute|hour|sec|min|hr)s?)?$/i);
+
+        if (match) {
+            const value = match[1] ? parseInt(match[1]) : 1; // Default to 1 if no value is specified
+            const unit = match[2] ? match[2].toLowerCase() : null;
+
+            switch (unit) {
+                case 'sec':
+                case 'second':
+                case 'seconds':
+                    return `*/${value} * * * * *`;
+                case 'min':
+                case 'minute':
+                case 'minutes':
+                    return `0 */${value} * * * *`;
+                case 'hr':
+                case 'hour':
+                case 'hours':
+                    return `0 0 */${value} * * *`;
+                default:
+                    return null;
+            }
+        }
+
+        return null;
     }
 
 }
--- tests/unit/components/datetime.test.js
+++ tests/unit/components/datetime.test.js
@@ -1,5 +1,7 @@
 
 const schedule = require('node-schedule');
+schedule.scheduleJob = jest.fn();
+
 const moment = require('moment');
 jest.mock('./../../../src/fluent', () => require('./../../__mocks__/fluent'));
 jest.mock('./../../../src/utils/logger');
@@ -12,10 +14,6 @@ const ComponentHelper = require('./../../helpers/component_helper.js');
 
 describe('Datetime setup', () => {
 
-    beforeEach(() => {
-        schedule.scheduleJob = jest.fn();
-    });
-
     it('has setup scheduler', () => {
         new DatetimeComponent(Fluent);
         expect(schedule.scheduleJob).toHaveBeenCalledTimes(3);
@@ -27,10 +25,9 @@ describe('Datetime setup', () => {
 });
 
 
-describe('Datetime constraints', () => {
+describe('Datetime constraints for "day is"', () => {
     
     let datetime;
-
     beforeEach(() => {
         schedule.scheduleJob = jest.fn();
         datetime = new DatetimeComponent(Fluent);
@@ -39,7 +36,6 @@ describe('Datetime constraints', () => {
     it('day is today', () => {
         const fullDayName = moment().format('dddd');
         const abbreviatedDayName = moment().format('ddd');
-
         expect(datetime.constraints().day.is(fullDayName)).toBe(true);
         expect(datetime.constraints().day.is(abbreviatedDayName)).toBe(true);
     });
@@ -51,10 +47,60 @@ describe('Datetime constraints', () => {
         expect(result).toBe(true);
     });
 
+    it('supports weekend and weekdays', () => {
+        expect(datetime.constraints().day.is(['weekday', 'weekend'])).toBe(true);
+    });
+
     it('is negative if not today', () => {
         const tomorrowDayName = moment().add(1, 'days').format('dddd');
         const result = datetime.constraints().day.is(tomorrowDayName);
         expect(result).toBe(false);
+    });
+
+    it('fails if the passed date is unknown', () => {
+        const result = datetime.constraints().day.is('zzz');
+        expect(result).toBe(false);
+    });
+
+    it('fails if the passed date is a number', () => {
+        const result = datetime.constraints().day.is(111);
+        expect(result).toBe(false);
+    });
+
+});
+
+
+
+describe('Datetime constraints for "between"', () => {
+
+    let datetime;
+    beforeEach(() => {
+        schedule.scheduleJob = jest.fn();
+        datetime = new DatetimeComponent(Fluent);
+    });
+
+    it('between is true when between times', () => {
+        const start = moment().clone().subtract(1, 'minute').format('HH:mm');
+        const end = moment().clone().add(1, 'minute').format('HH:mm');
+        expect(datetime.constraints().time.between(start, end)).toBe(true);
+    });
+
+    it('between is false when out of time', () => {
+        const start = moment().clone().subtract(2, 'minute').format('HH:mm');
+        const end = moment().clone().subtract(1, 'minute').format('HH:mm');
+        expect(datetime.constraints().time.between(start, end)).toBe(false);
+    });
+
+    it('between is true when exactly now', () => {
+        const start = moment().format('HH:mm');
+        const end = moment().clone().add(1, 'minute').format('HH:mm');
+        expect(datetime.constraints().time.between(start, end)).toBe(true);
+    });
+
+    it('between fails if the between start or end date is not valid', () => {
+        const any = moment().format('HH:mm');
+        expect(datetime.constraints().time.between('zz:aa', any)).toBe(false);
+        expect(datetime.constraints().time.between(any, 'zz:aa')).toBe(false);
     });
 
 });
@@ -62,5 +108,85 @@ describe('Datetime constraints', () => {
 
 describe('Datetime triggers', () => {
 
+    let Scenario;
+    let datetime;
+    
+    beforeEach(() => {
+        datetime = new DatetimeComponent(Fluent);
+        Scenario = ComponentHelper.ScenarioAndEvent(datetime);
+
+        schedule.scheduleJob = jest.fn((schedule, callback) => {
+            callback();
+        });   
+    });
+
+    it('time event triggers', () => {
+        datetime.triggers(Scenario).time.is('10:00');
+        datetime.event().emit('datetime.time', '10:00');
+        expect(Scenario.assert).toHaveBeenCalled();
+    });
+
+    it('different time event does not trigger', () => {
+        datetime.triggers(Scenario).time.is('10:00');
+        datetime.event().emit('datetime.time', '10:01');
+        expect(Scenario.assert).not.toHaveBeenCalled();
+    });
+
+    it('every second and different seconds', () => {
+        datetime.triggers(Scenario).time.every('second');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('*/1 * * * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('1 second');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('*/1 * * * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('10 seconds');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('*/10 * * * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('60 sec');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('*/60 * * * * *', expect.anything());
+
+        expect(Scenario.assert).toHaveBeenCalledTimes(4);
+    });
+
+    it('every minute and different minutes', () => {
+        datetime.triggers(Scenario).time.every('minute');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 */1 * * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('1 minute');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 */1 * * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('10 minutes');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 */10 * * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('60 mins');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 */60 * * * *', expect.anything());
+
+        expect(Scenario.assert).toHaveBeenCalledTimes(4);
+    });
+
+    it('every hour and different hours', () => {
+        datetime.triggers(Scenario).time.every('hour');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 0 */1 * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('1 hr');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 0 */1 * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('10 hour');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 0 */10 * * *', expect.anything());
+
+        datetime.triggers(Scenario).time.every('60 hours');
+        expect(schedule.scheduleJob).toHaveBeenCalledWith('0 0 */60 * * *', expect.anything());
+
+        expect(Scenario.assert).toHaveBeenCalledTimes(4);
+    });
+
+    it('throws an error if the time is no valid', () => {
+        expect(() => datetime.triggers(Scenario).time.every('1hou')).toThrow(Error);
+        expect(() => datetime.triggers(Scenario).time.every('1 minz')).toThrow(Error);
+        expect(() => datetime.triggers(Scenario).time.every('1')).toThrow(Error);
+        expect(() => datetime.triggers(Scenario).time.every('minz')).toThrow(Error);
+
+        expect(Scenario.assert).not.toHaveBeenCalled();
+    });
 
 });
